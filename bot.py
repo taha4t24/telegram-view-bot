b677028fc0cc1366ab02bb1b2a224d7b4f58916e
taha4t24 <farajvandtaha97@gmail.com>
2026-06-03 19:13:09 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,101 @@
+from telegram import Update
 
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes
+)
+
+from config import *
+from database import *
+from keyboards import *
+
+# =========================
+# استارت
+# =========================
+
+async def start(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+):
+
+    user_id = update.effective_user.id
+
+    add_user(user_id)
+
+    await update.message.reply_text(
+        """
+به ربات تلگرامی Pulse+SP خوش آمدید
+
+⬅️ به منوی اصلی بازگشتید
+        """,
+        reply_markup=main_keyboard
+    )
+
+# =========================
+# حساب من
+# =========================
+
+async def my_account(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+):
+
+    user_id = update.effective_user.id
+
+    balance = get_balance(user_id)
+
+    await update.message.reply_text(
+        f"""
+👤 حساب شما
+
+🆔 آیدی:
+{user_id}
+
+💰 موجودی:
+{balance:,} تومان
+        """
+    )
+
+# =========================
+# پنل ادمین
+# =========================
+
+async def admin(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+):
+
+    if update.effective_user.id != ADMIN_ID:
+        return
+
+    await update.message.reply_text(
+        "👑 پنل مدیریت",
+        reply_markup=admin_panel
+    )
+
+# =========================
+# اجرای ربات
+# =========================
+
+app = ApplicationBuilder().token(
+    BOT_TOKEN
+).build()
+
+app.add_handler(
+    CommandHandler(
+        "start",
+        start
+    )
+)
+
+app.add_handler(
+    CommandHandler(
+        "admin",
+        admin
+    )
+)
+
+print("Bot Started...")
+
+app.run_polling()
