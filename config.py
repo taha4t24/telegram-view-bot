b198caa10bc647f58adda16ebf8620f8843dcd9f
taha4t24 <farajvandtaha97@gmail.com>
2026-06-03 19:13:45 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,27 @@
+# =========================
+# تنظیمات ربات
+# =========================
 
+BOT_TOKEN = "8614770879:AAFzNlcPHwGowbtYmCz3hUT_32YRPyp1WXY"
+
+API_KEY = "jEv69xDL1XRTZnrB"
+
+ADMIN_ID = 8019491735
+
+BASE_URL = "https://api.power-tel.ir/apic.php"
+
+# =========================
+# کارت بانکی
+# =========================
+
+CARD_OWNER = "طاها فرجوند"
+
+CARD_NUMBER = "6219861952393923"
+
+# =========================
+# تنظیمات مالی
+# =========================
+
+MIN_CHARGE = 50000
+
+CHARGE_FEE_PERCENT = 5
