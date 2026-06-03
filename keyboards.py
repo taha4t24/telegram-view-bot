4cafcdff2596d36c367cb4b8add8b528447c5380
taha4t24 <farajvandtaha97@gmail.com>
2026-06-03 19:14:47 +0330

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,1 +1,92 @@
+from telegram import (
+    ReplyKeyboardMarkup,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
 
+# =========================
+# منوی اصلی کاربران
+# =========================
+
+main_keyboard = ReplyKeyboardMarkup(
+    [
+        ["📈 سفارش بازدید", "🤖 سفارش بازدید خودکار"],
+
+        ["👤 حساب من", "💰 افزایش موجودی"],
+
+        ["💸 انتقال سکه", "📦 سفارشات فعال"],
+
+        ["📜 خرید های من", "🛠 پشتیبانی"]
+    ],
+    resize_keyboard=True
+)
+
+# =========================
+# پنل مدیریت
+# =========================
+
+admin_panel = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(
+                "📊 داشبورد",
+                callback_data="dashboard"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                "💳 موجودی API",
+                callback_data="api_balance"
+            ),
+
+            InlineKeyboardButton(
+                "📦 سفارش ها",
+                callback_data="orders"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                "👤 جستجوی کاربر",
+                callback_data="search_user"
+            ),
+
+            InlineKeyboardButton(
+                "➕ شارژ کاربر",
+                callback_data="charge_user"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                "🚫 بن کاربر",
+                callback_data="ban_user"
+            ),
+
+            InlineKeyboardButton(
+                "✅ آنبن کاربر",
+                callback_data="unban_user"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                "💰 سود امروز",
+                callback_data="today_profit"
+            ),
+
+            InlineKeyboardButton(
+                "💵 سود کل",
+                callback_data="total_profit"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                "📨 ارسال همگانی",
+                callback_data="broadcast"
+            )
+        ]
+    ]
+)
