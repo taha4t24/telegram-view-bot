86002764be51d8983815ec8410efb192f0b567d8
taha4t24 <farajvandtaha97@gmail.com>
2026-06-04 14:54:05 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -120,31 +120,27 @@ async def buttons(
 
     text = update.message.text
 
-    # =========================
-# انتخاب سرویس
-# =========================
+    # انتخاب سرویس
+    if context.user_data.get("waiting_service"):
 
-if context.user_data.get("waiting_service"):
+        if text not in SERVICES:
 
-    if text not in SERVICES:
+            await update.message.reply_text(
+                "❌ سرویس معتبر نیست."
+            )
+
+            return
+
+        context.user_data["service"] = text
+
+        context.user_data["waiting_service"] = False
+        context.user_data["waiting_link"] = True
 
         await update.message.reply_text(
-            "❌ سرویس معتبر نیست."
+            "🔗 لینک پست را ارسال کنید."
         )
 
         return
-
-    context.user_data["service"] = text
-
-    context.user_data["waiting_service"] = False
-    context.user_data["waiting_link"] = True
-
-    await update.message.reply_text(
-        "🔗 لینک پست را ارسال کنید."
-    )
-
-    return
-
 
     # دریافت مبلغ شارژ
     if context.user_data.get("waiting_charge_amount"):
@@ -209,24 +205,18 @@ if context.user_data.get("waiting_service"):
     # حساب من
     if text == "👤 حساب من":
 
-        await my_account(
-            update,
-            context
-        )
+        await my_account(update, context)
 
     # افزایش موجودی
     elif text == "💰 افزایش موجودی":
 
-        await increase_balance(
-            update,
-            context
-        )
+        await increase_balance(update, context)
 
     # سفارش بازدید
-elif text == "📈 سفارش بازدید":
+    elif text == "📈 سفارش بازدید":
 
-    await update.message.reply_text(
-        """
+        await update.message.reply_text(
+            """
 سرویس مورد نظر را انتخاب کنید:
 
 بازدید ارزان
@@ -235,10 +225,10 @@ elif text == "📈 سفارش بازدید":
 بازدید تبلیغاتی
 
 نام سرویس را ارسال کنید.
-        """
-    )
+            """
+        )
 
-    context.user_data["waiting_service"] = True
+        context.user_data["waiting_service"] = True
 
     # سفارش خودکار
     elif text == "🤖 سفارش بازدید خودکار":
@@ -276,19 +266,14 @@ elif text == "📈 سفارش بازدید":
 💰 تعرفه محصولات به شرح زیر میباشد 👇
 
 💬 بازدید ارزان #آنی : 600
-
 ⚡️ پنل اختصاصی دقیق #آنی 👀 : 750
-
 🇺🇸 بازدید فیک #آنی : 800
-
 📊 بازدید مخصوص تبلیغات در کانال (با لینک) : 850
 
 ━━━━━━━━━━━━━━
 
 ❤️ ریکشن : 10000
-
 📊 نظرسنجی : 25000
-
 👍 لایک : 24000
 
 ━━━━━━━━━━━━━━
@@ -303,7 +288,7 @@ elif text == "📈 سفارش بازدید":
         await update.message.reply_text(
             "📩 برای پشتیبانی به ادمین پیام دهید."
         )
-
+        
 # =========================
 # دریافت رسید
 # =========================
